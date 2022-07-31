a4f146c6dca8ab7aacdda9b3292389c359143087
Md Touhid <mohammedtouhid007@gmail.com>
2022-07-31 18:26:15 +0600

    Added two section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1140px',
+    },
     extend: {},
   },
   plugins: [],
